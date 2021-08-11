68c46d56262b2baf87fa95a1a820ca7e2dff9704
SRC0747 <srcsrk1998@gmail.com>
2021-08-11 20:46:54 +0530

    added: Adding Part-time employee hours and calculating daily Employee Wage.

--- src/com/EmpWageComputation/EmployeeWage.java
+++ src/com/EmpWageComputation/EmployeeWage.java
@@ -4,13 +4,18 @@ public class EmployeeWage {
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation Problem.");
-        int IS_FULL_TIME=1;
+        int IS_FULL_TIME=2;
+        int IS_PART_TIME=1;
         int EMP_RATE_PER_HOUR=20;
         int empHrs=0;
         int empWage=0;
-        double empCheck = Math.floor(Math.random()*10)%2;
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present.");
+        double empCheck = Math.floor(Math.random()*10)%3;
+        if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Part-time Present.");
+            empHrs=4;
+        }
+        else if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Full-time Present.");
             empHrs=8;
         }
         else {
