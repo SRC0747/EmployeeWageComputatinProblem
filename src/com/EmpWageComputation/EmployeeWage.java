914861934dc17d9892ef2063d6e1aca409be92b7
SRC0747 <srcsrk1998@gmail.com>
2021-08-11 20:53:30 +0530

    added: Calculating Daily Employee Wage for a month havong 20 working Days.

--- src/com/EmpWageComputation/EmployeeWage.java
+++ src/com/EmpWageComputation/EmployeeWage.java
@@ -7,19 +7,26 @@ public class EmployeeWage {
         int IS_FULL_TIME=2;
         int IS_PART_TIME=1;
         int EMP_RATE_PER_HOUR=20;
+        int day=1;
         int empHrs=0;
         int empWage=0;
-        int empCheck = (int) Math.floor(Math.random()*10)%3;
-        switch (empCheck){
-            case 2:
-                empHrs=8;
-                break;
-            case 1:
-                empHrs=4;
-                break;
-            default:
-                empHrs=0;
-                break;
+        int workingDays=20;
+        int empCheck=(int) Math.floor(Math.random()*10)%3;
+        for(day=1; day<=workingDays; day++) {
+            switch (empCheck) {
+                case 2:
+                    System.out.println("Employee Full-time Present.");
+                    empHrs = 8;
+                    break;
+                case 1:
+                    System.out.println("Employee Part-time Present.");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is absent.");
+                    empHrs = 0;
+                    break;
+            }
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage is:"+empWage);
